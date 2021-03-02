81e54da081dfd318e30bc96f013e54ab9340bda4
Yuya Yamashita <yama.98904@gmail.com>
2021-03-02 20:14:16 +0900

    Initial commit

---
+++ sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>今日は何の日</title>
+</head>
+
+<body>
+    <h1></h1>
+    <p>今日は<?php echo date('Y年m月d日'); ?>ですねん</p>
+</body>
+
+</html>
